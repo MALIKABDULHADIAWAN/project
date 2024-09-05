a3f78f37468d6fa12dc731d213e9ea7f1868d8d3
MALIK ABDUL HADI AWAN <168334969+MALIKABDULHADIAWAN@users.noreply.github.com>
2024-09-05 11:22:46 +0000

    upgraded some things

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
-// alert("Arham this is special for you click ok")
-// alert(" click ok again😊😎")
+alert("Arham this is special for you click ok")
+alert(" click ok again😊😎")
 
 
-// for (let i = 1; i > 0; i++) {
-//     console.log(i)
-// }
+for (let i = 1; i > 0; i++) {
+    console.log(i)
+}
